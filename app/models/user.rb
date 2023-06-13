479b043c5354dedb15bcad2c0dac557a231f74b2
tguardado1994 <111324014+tguardado1994@users.noreply.github.com>
2023-06-13 12:46:39 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,13 +28,13 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true
 
-  scope :invite_not_expired, -> { where('invitation_expiration > ?', DateTime.now) }
-  scope :invite_token_is, ->(invitation_token) { where(invitation_token: invitation_token) }
+  # scope :invite_not_expired, -> { where('invitation_expiration > ?', DateTime.now) }
+  # scope :invite_token_is, ->(invitation_token) { where(invitation_token: invitation_token) }
 
   # Callbacks
-  before_create :generate_invitation_token
-  before_save :generate_invitation_token, if: :will_save_change_to_invitation_token?
-  after_commit :invite_user, if: :saved_change_to_invitation_token?
+  # before_create :generate_invitation_token
+  # before_save :generate_invitation_token, if: :will_save_change_to_invitation_token?
+  # after_commit :invite_user, if: :saved_change_to_invitation_token?
 
   def generate_token!(ip)
     token = Token.create(
@@ -45,30 +45,30 @@ class User < ApplicationRecord
     )
   end
 
-  def generate_invitation_token
-    self.invitation_expiration = DateTime.current + 7.day
-    loop do
-      # Once we have a random, test whether it is unique in the DB
-      self.invitation_token = SecureRandom.alphanumeric(15)
-      break unless self.class.exists?(invitation_token: invitation_token)
-    end
-  end
+  # def generate_invitation_token
+  #   self.invitation_expiration = DateTime.current + 7.day
+  #   loop do
+  #     # Once we have a random, test whether it is unique in the DB
+  #     self.invitation_token = SecureRandom.alphanumeric(15)
+  #     break unless self.class.exists?(invitation_token: invitation_token)
+  #   end
+  # end
 
-  def invitation_accepted_at!
-    update(invitation_accepted: true, invitation_token: nil, invitation_expiration: nil)
-  end
+  # def invitation_accepted_at!
+  #   update(invitation_accepted: true, invitation_token: nil, invitation_expiration: nil)
+  # end
 
-  def invitation_link
-    throw 'Environment Variable Not Found Error' if Rails.application.credentials.invitation[:url].nil?
+  # def invitation_link
+  #   throw 'Environment Variable Not Found Error' if Rails.application.credentials.invitation[:url].nil?
 
-    url = Rails.application.credentials.invitation[:url]
-    "#{url}#{invitation_token}"
-  end
+  #   url = Rails.application.credentials.invitation[:url]
+  #   "#{url}#{invitation_token}"
+  # end
 
-  def invite_user
-    # Email the user a link with the invitation_token
-    InvitationWorker.perform_async(id)
-  end
+  # def invite_user
+  #   # Email the user a link with the invitation_token
+  #   InvitationWorker.perform_async(id)
+  # end
 
   def name
     "#{first_name} #{last_name}"
